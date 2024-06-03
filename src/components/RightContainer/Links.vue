7d55206baa73688fdd501c1ba2d52740064839f8
Woo0ow <whoareyouxp@gmail.com>
2024-06-03 14:06:57 +0000

    2024.6.3 update

--- src/components/RightContainer/Links.vue
+++ src/components/RightContainer/Links.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="links">
         <div class="m-title pt-4 pb-5 mb-2">友链</div>
+        <div>
+            github : <a href="https://github.com/Woo0ow">https://github.com/Woo0ow</a>
+        </div>
     </div>
 </template>
 <script>
