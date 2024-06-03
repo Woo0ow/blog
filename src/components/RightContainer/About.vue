77c67bf579909e64cf283ebc5a98456bfb1730a3
Woo0ow <whoareyouxp@gmail.com>
2024-06-03 16:49:32 +0000

    2024.6.4 update

--- src/components/RightContainer/About.vue
+++ src/components/RightContainer/About.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="about">
         <div class="m-title pt-4 pb-5 mb-2">关于</div>
+        <div>
+            github : <a href="https://github.com/Woo0ow">https://github.com/Woo0ow</a>
+        </div>
     </div>
 </template>
 <script>
